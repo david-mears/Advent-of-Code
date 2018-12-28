770b1734fea0a7a7c6103b1afc9d416998be9c60
David Mears <kieukmail@gmail.com>
2018-12-28 17:00:04 +0000

    Added change_string_once method

--- Day_5/5-Dec-1-second.rb
+++ Day_5/5-Dec-1-second.rb
@@ -2,4 +2,35 @@ input = ''
 File.open("Day_5/input5").each do |line|
   input = line
 end
-puts input
+
+def change_string_once(string)
+  
+  to_change = []
+  string.chars.each.with_index do |letter, index|
+    if to_change == [] && index+1 != string.length
+      if string[index] != string[index+1] && string[index].downcase == string[index+1].downcase
+        to_change.push(index)
+      end
+    end
+    # We should have one number in to_change.
+  end
+  
+  if to_change != []
+    i = to_change[0]
+  else
+    return string
+  end
+  
+  if i == 0
+    return string[2..-1]
+  else
+    return string[0..i-1] + string[i+2..-1]
+  end
+  
+end
+
+puts change_string_once(gets.chomp)
+
+previous_string = input 
+until previous_string == input
+end
